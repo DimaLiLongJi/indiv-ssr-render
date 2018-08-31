1d898e49e56b82ccff2f19618fc6d1a0120460b3
lilongji <woshililongji@163.com>
2018-08-31 18:25:04 +0800

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// ejs 相关
+app.engine('.html', require('ejs').__express);
+
+app.set('view engine', 'html');
+
+// 静态文件
+app.set('views', './');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/demo', (req, res, next) => {
+  res.render('index.html');
+});
+
+app.listen(1234);
+console.log(`listening port 1234`);
+
+
+
+module.exports = app;
