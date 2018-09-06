496a570a2c9a51902c2c258e9aebdaf22a50d7c1
lilongji <woshililongji@163.com>
2018-09-06 17:51:15 +0800

    还差组件render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const { factoryModule, factoryCreator } = require('indiv');
-const { buildPath, findRoutes, findComponent } = require('./getComponent');
-
 const Window = require('window');
+
+const Compile = require('./compile');
+const { factoryModule, factoryCreator } = require('indiv');
+const { buildPath, findRoutes, findComponent, mountComponent, instantiateComponent } = require('./buildComponent');
+const domToString = require('./domToString');
+
 
 const window = new Window();
 const document = window.document;
-const Compile = require('./compile');
 
 function renderToString(url, routes, RootModule) {
   const pathList = buildPath(url);
@@ -14,19 +16,29 @@ function renderToString(url, routes, RootModule) {
   const componentList = findComponent(routesList, rootModule);
 
   // render component
-  const documentFragment = document.createDocumentFragment();
+  const documentFragment = document.createElement('div');
+    // const com = instantiateComponent(component, rootModule);
+    // if (index === 0) {
+    //   new Compile(com, documentFragment);
+    //   mountComponent(com, documentFragment, rootModule);
+    // } else {
+    //   const el = documentFragment.querySelectorAll('router-render')[index - 1];
+    //   new Compile(com, el);
+    //   mountComponent(com, el, rootModule);
+    // }
   componentList.forEach((component, index) => {
-    const com = factoryCreator(component, rootModule);
     if (index === 0) {
-      new Compile(com, documentFragment);
+      console.log(44444, documentFragment);
+      const com = instantiateComponent(component, rootModule, documentFragment);
     } else {
-      const el = documentFragment.querySelectorAll('router-render')[0];
-      new Compile(com, el);
+      const el = documentFragment.querySelectorAll('router-render')[index - 1];
+      console.log(555555, el);
+      const com = instantiateComponent(component, rootModule, el);
     }
   });
-
-  console.log(111111111, documentFragment);
-  return '11';
+  const domString = domToString(documentFragment);
+  console.log(1111, domString);
+  return domString.replace(/^(\<div\>)/g, '').replace(/(\<\/div\>$)/g, '');
 }
 
 module.exports = renderToString;
